0ad51cf9f017324d88c6722f004f4bf16615c541
giadadesanctis <dsgiada4@gmail.com>
2025-04-15 18:00:38 +0200

    robe di js

--- privatearea/js/calendario_script.js
+++ privatearea/js/calendario_script.js
@@ -75,32 +75,3 @@ function inizializzaCalendario(){
       });
       calendar.render();
 }
-
-function inizializzaCalendarioHome(){
-    const isMobile = window.innerWidth < 768;
-
-    var calendarEl = document.getElementById('calendar');
-    var calendar = new FullCalendar.Calendar(calendarEl, {
-        initialView: 'listMonth', 
-        locale: 'it',
-        validRange: { start: new Date() }, 
-        firstDay: 1, 
-        nowIndicator: true,
-
-        headerToolbar:false,
-        views: {
-            listDay: {
-                buttonText: 'Oggi'
-            }
-        },
-
-        // Imposta gli eventi per oggi (dobbiamo sostituirli con quelli reali)
-        events: [
-            { title: 'Compito 1', start: '2025-04-13T10:00:00' },
-            { title: 'Compito 2', start: '2025-04-13T14:00:00' }
-        ]
-    });
-
-    calendar.render();
-
-}
