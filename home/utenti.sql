f796e0c39c359dd6fe8efeff9ee4d7b2a0d30888
cristiana.dabbruzzo <cristiana.dabbruzzo@gmail.com>
2025-04-26 15:22:30 +0200

    file utenti.sql

---
+++ home/utenti.sql
@@ -0,0 +1,27 @@
+/*
+("host=localhost 
+dbname=users 
+user=postgres 
+password=biar")
+
+Ho creato un database users su PGADMIN (user e password li ho impostati quando ho collegato postgres a XAMPP)
+E poi ho creato la tabella users
+*/ 
+CREATE TABLE users(
+    nome varchar(50) NOT NULL, 
+    cognome varchar(50) NOT NULL, 
+    email varchar(50) PRIMARY KEY, -- ho messo mail primary key così non si può usare più volte la stessa mail: il controllo viene fatto da registrati.php
+    username varchar(50) NOT NULL,
+    password varchar(50) NOT NULL,
+)
+
+
+/* 
+per far par funzionare tutto su windows dal terminale di vscode: 
+- entrare nella cartella home 
+- fare php -S localhost: 8000 
+- e poi nel browser: http://localhost:8000/homepage.html
+- e poi registrati 
+
+- mancano SFONDO E LOGO 
+
