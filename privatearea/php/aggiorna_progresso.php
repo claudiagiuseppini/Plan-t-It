fb4c56a9c8a803d8aef7abc5478e998f4d4f8d84
claudiagiuseppini <giuseppinic@gmail.com>
2025-05-25 10:06:31 +0200

    tutti possono cambiare il progresso (sia proprietario compito che altri)

--- privatearea/php/aggiorna_progresso.php
+++ privatearea/php/aggiorna_progresso.php
@@ -4,27 +4,17 @@ session_start();
 // connessione al DB
 require 'db_connect.php';
 
-// recupero lo username dalla sessione
-$username = isset($_SESSION['username']) ? $_SESSION['username'] : null;
-
-if ($username === null) {
-    http_response_code(401);
-    echo json_encode(["error" => "Utente non autenticato"]);
-    exit;
-}
-
 // recupero id e progresso del compito
 $compito_id = $_POST['id'] ?? null;
 $progresso = $_POST['progresso'] ?? null;
 
 
 //compongo la query
-$query = "UPDATE compiti SET progresso = $1 WHERE id = $2 AND utente = $3";
+$query = "UPDATE compiti SET progresso = $1 WHERE id = $2";
 
 $result = pg_query_params($conn, $query, [
     $progresso,
-    $compito_id,
-    $username
+    $compito_id
 ]);
 
 if ($result) {
